4a3b30cab1b39ac37b67d97e8d5af99ada29117b
arsshuz <arshadimb786@gmail.com>
2026-04-27 19:07:01 +0530

    add vercel.json file again

--- FRONTEND/src/components/AdminRoute.jsx
+++ FRONTEND/src/components/AdminRoute.jsx
@@ -1,14 +1,14 @@
- import {Navigate} from "react-router-dom"
- import {useAuth} from "../context/AuthContext"
+import { Navigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
 
- function AdminRoute({children}){
-    const {user} =useAuth()
-    if(!user){
-        return <Navigate to="/login" replace/>
+function AdminRoute({ children }) {
+    const { user } = useAuth()
+    if (!user) {
+        return <Navigate to="/login" replace />
     }
-    if(user.role !== "admin"){
-        return <Navigate to="/jobs" replace/>
+    if (user.role !== "admin") {
+        return <Navigate to="/" replace />
     }
     return children
- }
- export default AdminRoute
+}
+export default AdminRoute
